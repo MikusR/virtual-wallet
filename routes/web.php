6d4b8f288ef25b479de1d37bba56ecb8ac93ce51
MikusR <mikus.rakis@gmail.com>
2024-02-20 17:16:41 +0200

    name the login route 'login' so on unauthentificated auto redirects to login.

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('register', [RegisterController::class, 'create'])->middleware('guest
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
 
 Route::post('logout', [SessionController::class, 'destroy'])->middleware('auth');
-Route::get('login', [SessionController::class, 'create'])->middleware('guest');
+Route::get('login', [SessionController::class, 'create'])->middleware('guest')->name('login');
 Route::post('login', [SessionController::class, 'store'])->middleware('guest');
 
 Route::get('/wallets/create', [WalletController::class, 'create'])->middleware('auth');
